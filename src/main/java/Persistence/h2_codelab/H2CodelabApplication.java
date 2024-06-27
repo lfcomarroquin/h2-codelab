42cd54df2d8128c7605b1e5e7276043d1f120085
Luis Francisco Marroquin Ortiz <lfortiz.agt@esieduc.org>
2024-06-27 12:54:42 -0600

    H2 codelab - Persistence

---
+++ src/main/java/Persistence/h2_codelab/H2CodelabApplication.java
@@ -0,0 +1,13 @@
+package Persistence.h2_codelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2CodelabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2CodelabApplication.class, args);
+	}
+
+}
